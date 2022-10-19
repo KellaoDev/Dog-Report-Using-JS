e6d65e3b0e5fa4ae7e7c48a886a342a18beb85ee
Kellao <67332135+KellaoDev@users.noreply.github.com>
2022-10-19 11:32:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function Texto01() {
+    document.getElementById("center").innerHTML = "The Lhasa Apso are very furry, small dogs (from 24 to 25 cm in height), calm and with a strong personality. Despite their small size, Lhasa Apsos can be watchdogs as they are always alert and bark in the presence of strangers. They are very protective and when well trained they are extremely obedient. Despite the similarity, the Lhasas have a longer muzzle than the Shih Tzu and a slightly narrower skull."
+    document.getElementById('photo').src = "https://petpillow.com.br/wp-content/uploads/2020/08/image14-768x512.jpg"
+}
+function Texto02() {
+    document.getElementById("center").innerHTML = "One of the most charismatic dog breeds, the Golden Retriever is a naturally hunting dog and also an excellent guide dog. Known for its very sweet and affectionate temperament, the Golden has a lot of energy and enjoys playing sports, including swimming. Dogs of this breed can be a little anxious and often ask for affection even from strangers. It is important that the Golden is trained and that he exercises daily, as his excess energy can cause even small accidents inside the house."
+    document.getElementById('photo').src = "https://petpillow.com.br/wp-content/uploads/2020/08/image8.png"
+}
+function Texto03() {
+    document.getElementById("center").innerHTML = "The famous “salsichinha” dog is a very common breed in Brazil for many years. They are dogs that have a great sense of smell and are good diggers. Despite their small size (they are 30 to 35 cm tall, on average), they usually have a strong temperament and, therefore, it is recommended that they be trained. They are intelligent and have short and long hair variations. Dachshunds have a tendency to bond with one person in particular and end up being jealous of their owners."
+    document.getElementById('photo').src = "https://petpillow.com.br/wp-content/uploads/2020/08/image9.jpg"
+}
+
+window.alert("ANIMAIS PROBLEMATICOS!")
+
